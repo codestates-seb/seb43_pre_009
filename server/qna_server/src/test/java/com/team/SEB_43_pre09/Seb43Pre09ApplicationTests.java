e72c8c0ed1d4218c9c52391172e59a534ddf9e8b
taegyun <taegyun0903@gmail.com>
2023-04-17 13:02:02 +0900

    spring 추가

---
+++ server/qna_server/src/test/java/com/team/SEB_43_pre09/Seb43Pre09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.SEB_43_pre09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb43Pre09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
