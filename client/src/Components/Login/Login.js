544c711443a7e67cee0d1bebf07175113159c2ab
정제우 <wpdn3825@naver.com>
2023-04-19 22:58:13 +0900

    로그인 쿠키 구현

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,24 +1,89 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const LoginWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  const navigate = useNavigate();
+
+  const loginUser = async (email, password) => {
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include', // 쿠키를 보내기 위한 옵션 설정
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const onChange = (event) => {
+    console.log(event.target.name, event.target.value);
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const data = await loginUser(email, password);
+      if (data.success) {
+        console.log('로그인 성공:', data);
+        setIsLoggedin(true);
+        navigate('/');
+      } else {
+        console.log('로그인 실패');
+        setIsLoggedin(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <LoginWrapper>
-      <h1>로그인창</h1>
-      <form>
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
-        <button type="submit">로그인인 ㅠㅠ</button>
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
       </form>
-      <p>
-        dont have an account?<Link to="/signup">Signup</Link>
-      </p>
-    </LoginWrapper>
+      {isLoggedin ? (
+        <p>로그인 성공</p>
+      ) : (
+        <p>
+          dont have an account? <Link to="/signup">Sign up</Link>
+        </p>
+      )}
+    </>
   );
 };
 
--- client/src/Components/Login/Signup.js
+++ client/src/Components/Login/Signup.js
@@ -1,7 +1,79 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const signupUser = async (email, password) => {
+    try {
+      const response = await fetch('http://localhost:3001/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include',
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const data = await signupUser(email, password);
+      if (data) {
+        console.log('회원가입 성공:', data);
+        // TODO: 회원가입 성공 시 처리할 로직 작성
+        navigate('/login');
+      } else {
+        console.log('회원가입 실패');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChange = (event) => {
+    console.log(event.target.name, event.target.value);
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
   return (
     <>
-      <h1>회원가입</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Sign Up" />
+      </form>
     </>
   );
 };
