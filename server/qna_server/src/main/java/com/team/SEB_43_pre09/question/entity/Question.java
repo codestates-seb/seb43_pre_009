3fc05226858af1d84fcb3bf6557325b790b2a78a
SEBBE43JUN <eguana111@gmail.com>
2023-04-21 17:47:40 +0900

    답변채택기능추가(04-21

--- server/qna_server/src/main/java/com/team/SEB_43_pre09/question/entity/Question.java
+++ server/qna_server/src/main/java/com/team/SEB_43_pre09/question/entity/Question.java
@@ -1,5 +1,6 @@
 package com.team.SEB_43_pre09.question.entity;
 
+import com.team.SEB_43_pre09.answer.entity.Answer;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long member_id;  //질문 ID
+    private long question_id;  //질문 ID
 
     @Column(nullable = false, length = 1000)
     private String question_title;  //질문 제목
@@ -33,7 +34,11 @@ public class Question {
     private long view_count;  //조회수
 
     @Column(nullable = false)
-    private long question_id;  //질문 작성자의 회원 ID
+    private long member_id;  //질문 작성자의 회원 ID
+
+    @OneToOne
+    @JoinColumn(name = "accepted_answer_id")
+    private Answer accepted_Answer;    //채택된 답변
 
     public Question(String title, String content, long memberId) {
         this.question_title = title;
@@ -42,6 +47,11 @@ public class Question {
         this.created_at = LocalDateTime.now();
         this.modified_at = LocalDateTime.now();
         this.view_count = 0;
+    }
+    //채택된 답변을 설정하는 메서드
+    public void setAcceptedAnswer(Answer answer) {
+        this.accepted_Answer = answer;
+        answer.setAnswer_status (Answer.AnswerStatus.ANSWER_ADOPT);
     }
 }
 
