ffca9ea10550eda9c2cd5d80a4b8d679294900b4
SEBBE43JUN <eguana111@gmail.com>
2023-04-24 08:54:03 +0900

    답변채택기능추가(04-23

--- server/qna_server/src/main/java/com/team/SEB_43_pre09/question/repository/QuestionRepository.java
+++ server/qna_server/src/main/java/com/team/SEB_43_pre09/question/repository/QuestionRepository.java
@@ -1,10 +1,13 @@
 package com.team.SEB_43_pre09.question.repository;
 
+import com.team.SEB_43_pre09.answer.entity.Answer;
 import com.team.SEB_43_pre09.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+    Optional<Question> findByAcceptedAnswer(Answer answer);
 }
