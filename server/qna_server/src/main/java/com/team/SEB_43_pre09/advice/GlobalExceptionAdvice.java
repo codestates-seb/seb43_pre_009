629fd4b755d9b7781c7efd29d78be2ad76ea9d8c
exertivestar <exertivestar@naver.com>
2023-04-25 00:36:39 +0900

    feat: ErrorResponse, GlobalExceptionAdvice 클래스 생성

---
+++ server/qna_server/src/main/java/com/team/SEB_43_pre09/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,98 @@
+package com.team.SEB_43_pre09.advice;
+
+import com.team.SEB_43_pre09.exception.BusinessLogicException;
+import com.team.SEB_43_pre09.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+/**
+ *
+ *
+ *  Controller 클래스의 예외를 처리할 GlobalExceptionAdvice 클래스
+ *
+ *
+ **/
+@Slf4j // 로깅에 대한 추상 레이어를 제공하는 애너테이션
+@RestControllerAdvice // Controller 클래스에서 발생하는 예외를 도맡아서 처리하겠다는 애너테이션
+public class GlobalExceptionAdvice {
+    /** Exception 핸들러 메서드 */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+
+    /** 서비스 계층의 비즈니스 로직 처리에서 발생하는 예외를 처리하는 것을 목적으로 하는 메서드 */
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode()
+                .getStatus()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
+                "Required request body is missing");
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
+                e.getMessage());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error("# handle Exception", e);
+        // TODO 애플리케이션의 에러는 에러 로그를 로그에 기록하고, 관리자에게 이메일이나 카카오 톡,
+        //  슬랙 등으로 알려주는 로직이 있는게 좋습니다.
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return response;
+    }
+}
--- server/qna_server/src/main/java/com/team/SEB_43_pre09/exception/BusinessLogicException.java
+++ server/qna_server/src/main/java/com/team/SEB_43_pre09/exception/BusinessLogicException.java
@@ -1,14 +1,21 @@
 package com.team.SEB_43_pre09.exception;
 
 import lombok.Getter;
+/**
+ *
+ *
+ *  서비스 계층에서 사용할 Custom Exception 입니다.
+ *
+ *
+ **/
+public class BusinessLogicException extends RuntimeException { // RuntimeException을 상속합니다.
 
-public class BusinessLogicException extends RuntimeException {
     @Getter
-    private ExceptionCode exceptionCode;
+    private ExceptionCode exceptionCode;  /** exceptionCode 를 멤버 변수로 지정하여 생성자를 통해 조금 더 구체적인 예외 정보들을 제공합니다. */
 
     public BusinessLogicException(ExceptionCode exceptionCode) {
         super(exceptionCode.getMessage());
         this.exceptionCode = exceptionCode;
-    }
+    } /** 상위 클래스인 RuntimeException 의 생성자(super)로 예외 메시지를 전달해 줍니다. */
 }
 
