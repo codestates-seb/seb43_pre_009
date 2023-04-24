648f412216763bf8eb4a8d542b97657b677153ab
정제우 <wpdn3825@naver.com>
2023-04-24 09:16:26 +0900

    fix : 유저프로필 수정모달 추가

--- client/src/Components/User/ProfileEdit.js
+++ client/src/Components/User/ProfileEdit.js
@@ -8,7 +8,7 @@ const ProfileEdit = () => {
   const [showChangeModal, setShowChangeModal] = useState(false);
 
   const openModalOut = () => {
-    setShowOutModal(true);
+    setShowOutModal(!showOutModal);
   };
 
   const closeModalOut = () => {
@@ -17,10 +17,10 @@ const ProfileEdit = () => {
   };
 
   const openModalChange = () => {
-    setShowChangeModal(true);
+    setShowChangeModal(!showChangeModal);
   };
 
-  const closeModalChange = () => {
+  const changePassword = () => {
     setShowChangeModal(false);
     navigate('/user');
   };
@@ -47,19 +47,22 @@ const ProfileEdit = () => {
       <div>
         <Btns onClick={openModalOut}>회원탈퇴</Btns>
         {showOutModal ? (
-          <ModalBackdrop>
-            <Modal>
+          <ModalBackdrop onClick={openModalOut}>
+            <Modal onClick={(event) => event.stopPropagation}>
+              <h3>진짜로 탈퇴하실건가요..</h3>
               <Btns onClick={closeModalOut}>진심탈퇴 하기</Btns>
+              <Btns onClick={openModalOut}>탈퇴 취소하기</Btns>
             </Modal>
           </ModalBackdrop>
         ) : null}
         <Btns onClick={openModalChange}>비밀번호 변경</Btns>
         {showChangeModal ? (
-          <ModalBackdrop>
-            <Modal>
+          <ModalBackdrop onClick={openModalChange}>
+            <Modal onClick={(event) => event.stopPropagation}>
               <h3>비밀번호 변경 모달</h3>
               <input type="text" />
-              <Btns onClick={closeModalChange}>비밀번호 변경하기</Btns>
+              <Btns onClick={changePassword}>비밀번호 변경하기</Btns>
+              <Btns onClick={openModalChange}>변경취소하기</Btns>
             </Modal>
           </ModalBackdrop>
         ) : null}
