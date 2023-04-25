17bda5ce002eef0db02dc753a9cadf75c7f448f7
KangHaSeong <shaawwert6044@gmail.com>
2023-04-25 16:55:39 +0900

    게시글 수정 및 접근 권한 등 주요내용 수정 완료

--- client/src/Components/Question/PostContents.js
+++ client/src/Components/Question/PostContents.js
@@ -12,14 +12,19 @@ import AnswerList from './AnswerList';
 const PostContents = () => {
   const { id } = useParams();
   const [post, setPost] = useState({});
+  const [user, setUser] = useState('');
+
   const navigate = useNavigate();
 
+  // 게시물을 가져오는 코드
   useEffect(() => {
     axios.get(`http://localhost:3001/questions/${id}`).then((res) => {
       setPost(res.data);
+      console.log(res.data);
     });
   }, [id]);
 
+  // 코드 블럭을 위한 함수
   const replaceCodeBlocks = (domNode) => {
     if (
       domNode.name === 'pre' &&
@@ -34,23 +39,48 @@ const PostContents = () => {
       );
     }
   };
-
+  // 게시물의 내용을 파싱하는 코드
   const parsedContent = parse(post.contents || '', {
     replace: replaceCodeBlocks,
   });
 
+  // ? 유저 정보를 쿠키에서 가져오는 코드 ===> 포스팅!
+  // 1. 쿠키 데이터를 상태에 담고
+  // 2. 쿠키 데이터를 찾고
+  // 3. 디코딩해서 볼 수 있는 형태로
+  useEffect(() => {
+    // user 닉네임을 받아오는 코드
+    const cookies = document.cookie.split('; ');
+    let memberName;
+    for (let i = 0; i < cookies.length; i++) {
+      const [name, value] = cookies[i].split('=');
+      if (name === 'memberName') {
+        memberName = value;
+        break;
+      }
+    }
+    // TODO memberName 변수에 쿠키에서 가져온 값을 할당 --------------------------
+    setUser(memberName);
+  }, []);
+
+  // 수정 버튼을 누르면 수정 페이지로 이동하는 코드
   const handleEdit = () => {
-    if (post.creator === localStorage.getItem('userId')) {
+    // 현재 지금 화면을 사용하고 있는 유저 : user === 포스트 작성자 : creator
+    if (user === post.creator) {
       navigate(`/question/edit/${id}`);
     } else {
-      alert('수정할 권한이 없습니다.');
+      alert('접근권한이 없습니다.');
     }
   };
 
+  // TODO 삭제 버튼을 누르면 삭제하는 코드 ---------------------------------------
   const handleDelete = () => {
-    axios.delete(`http://localhost:3001/questions/${id}`).then((res) => {
-      console.log(res);
-    });
+    if (user === post.creator) {
+      axios.delete(`http://localhost:3001/questions/${id}`);
+      navigate(`/question/edit/${id}`);
+    } else {
+      alert("You don't have permission to edit this question.");
+    }
     navigate('/question/*');
   };
 
