699797f5a76457a50a1beb9ea14261321996be7d
정제우 <wpdn3825@naver.com>
2023-04-26 17:30:49 +0900

    평판, 이메일 표기

--- client/src/Components/User/Profile.js
+++ client/src/Components/User/Profile.js
@@ -3,26 +3,40 @@ import styled from 'styled-components';
 import { Btns } from '../Layout/styled';
 import { useState, useEffect } from 'react';
 
-const ProfileWrapper = styled.div`
+const ProfileEditWrapper = styled.div`
   margin: 30px;
-  .info {
-  }
   .buttons {
     padding: 10px 0px 10px 0px;
   }
-  .info div {
-    padding: 60px;
-    border: 1px solid black;
-    border-radius: 5px;
+`;
+const ProfileWrapper = styled.div`
+  p {
+    color: gray;
   }
-  p,
-  h2 {
-    padding: 20px 0px 20px 0px;
+  h1,
+  p {
+    margin: 20px 0px 20px 0px;
+  }
+  .myinfo {
+    display: flex;
+    p {
+      padding-right: 30px;
+    }
   }
 `;
+
+const AboutmeWrapper = styled.div`
+  h3 {
+    margin: 20px 0px 20px 0px;
+  }
+`;
+
 const Profile = () => {
   const [memberName, setMemberName] = useState('');
   const [aboutme, setAboutMe] = useState('');
+  const [myemail, setMyEmail] = useState('');
+  const [myReputation, setMyReputation] = useState('');
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,6 +46,10 @@ const Profile = () => {
       row.startsWith('memberName')
     );
     const aboutmeCookie = cookies.find((row) => row.startsWith('aboutme'));
+    const emailCookie = cookies.find((row) => row.startsWith('email'));
+    const reputationCookie = cookies.find((row) =>
+      row.startsWith('reputation')
+    );
 
     if (memberNameCookie) {
       const memberNameValue = decodeURIComponent(
@@ -45,29 +63,45 @@ const Profile = () => {
       setAboutMe(aboutmeValue);
     }
 
+    if (emailCookie) {
+      const emailValue = decodeURIComponent(emailCookie.split('=')[1]);
+      setMyEmail(emailValue);
+    }
+
     if (!memberNameCookie || !aboutmeCookie) {
       navigate('/login');
+    }
+
+    if (reputationCookie) {
+      const reputationlValue = decodeURIComponent(
+        reputationCookie.split('=')[1]
+      );
+      setMyReputation(reputationlValue);
     }
   }, [navigate]);
 
   return (
-    <ProfileWrapper>
-      <div className="user">
+    <ProfileEditWrapper>
+      <ProfileWrapper>
         <h1>{memberName}님</h1>
-        <p>example@example.com</p>
-      </div>
+        <div className="myinfo">
+          {' '}
+          <p>✉️ : {myemail}</p>
+          <p>👍 : {myReputation}</p>
+        </div>
+      </ProfileWrapper>
       <div className="buttons">
         <Btns>profile</Btns>
         <Link to="/useredit">
           <Btns>edit</Btns>
         </Link>
       </div>
-      <div className="info">
-        <h2>About</h2>
-        <hr />
-        <div>{aboutme}</div>
-      </div>
-    </ProfileWrapper>
+
+      <AboutmeWrapper>
+        <h3>About</h3>
+        {aboutme ? <p>{aboutme}</p> : <p>자기소개가 없습니다.</p>}
+      </AboutmeWrapper>
+    </ProfileEditWrapper>
   );
 };
 
--- client/src/Components/User/ProfileEdit.js
+++ client/src/Components/User/ProfileEdit.js
@@ -10,9 +10,34 @@ const ProfileEditWrapper = styled.div`
   .buttons {
     padding: 10px 0px 10px 0px;
   }
-  p,
-  h2 {
-    padding: 20px 0px 20px 0px;
+`;
+const ProfileWrapper = styled.div`
+  p {
+    color: gray;
+  }
+  h1,
+  p {
+    margin: 20px 0px 20px 0px;
+  }
+  .myinfo {
+    display: flex;
+    p {
+      padding-right: 30px;
+    }
+  }
+`;
+const AboutmeWrapper = styled.div`
+  h3 {
+    margin: 20px 0px 20px 0px;
+  }
+`;
+
+const ChangeAboutme = styled.div``;
+const ChangeUser = styled.div`
+  border-top: 2px solid lightgray;
+  margin: 20px 0px 20px 0px;
+  .container {
+    margin: 20px 0px 20px 0px;
   }
 `;
 
@@ -25,6 +50,7 @@ const ProfileEdit = () => {
   const [aboutme, setAboutMe] = useState('');
   const [myemail, setMyEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [myReputation, setMyReputation] = useState('');
 
   useEffect(() => {
     // 쿠키에서 memberName과 aboutme 값을 가져와 상태값으로 설정
@@ -34,6 +60,9 @@ const ProfileEdit = () => {
     );
     const aboutmeCookie = cookies.find((row) => row.startsWith('aboutme'));
     const emailCookie = cookies.find((row) => row.startsWith('email'));
+    const reputationCookie = cookies.find((row) =>
+      row.startsWith('reputation')
+    );
 
     if (memberNameCookie) {
       const memberNameValue = decodeURIComponent(
@@ -50,6 +79,13 @@ const ProfileEdit = () => {
     if (emailCookie) {
       const emailValue = decodeURIComponent(emailCookie.split('=')[1]);
       setMyEmail(emailValue);
+    }
+
+    if (reputationCookie) {
+      const reputationlValue = decodeURIComponent(
+        reputationCookie.split('=')[1]
+      );
+      setMyReputation(reputationlValue);
     }
   }, []);
 
@@ -129,53 +165,64 @@ const ProfileEdit = () => {
 
   return (
     <ProfileEditWrapper>
-      <div>
+      <ProfileWrapper>
         <h1>{memberName}님</h1>
-        <p>example@example.com</p>
-      </div>
+        <div className="myinfo">
+          {' '}
+          <p>✉️ : {myemail}</p>
+          <p>👍 : {myReputation}</p>
+        </div>
+      </ProfileWrapper>
       <div className="buttons">
         <Btns onClick={goBack}>profile</Btns>
         <Btns>edit</Btns>
       </div>
-      <div>
-        <input
-          type="text"
-          value={newAboutMe}
-          onChange={aboutMeChange}
-          placeholder={`${aboutme}`}
-        />
-        <Btns onClick={handleAboutMeChange}>About Me 변경하기</Btns>
-      </div>
-      <div>
-        <Btns onClick={openModalOut}>회원탈퇴</Btns>
-        {showOutModal ? (
-          <ModalBackdrop onClick={openModalOut}>
-            <Modal onClick={(event) => event.stopPropagation()}>
-              <h3>진짜로 탈퇴하실건가요..</h3>
-              <Btns onClick={handleDelete(myemail)}>진심탈퇴 하기</Btns>
-              <Btns onClick={openModalOut}>탈퇴 취소하기</Btns>
-            </Modal>
-          </ModalBackdrop>
-        ) : null}
+      <AboutmeWrapper>
+        <ChangeAboutme>
+          <h3>About</h3>
+          <input
+            type="text"
+            value={newAboutMe}
+            onChange={aboutMeChange}
+            placeholder={`${aboutme}`}
+          />
+          <Btns onClick={handleAboutMeChange}>About Me 변경하기</Btns>
+        </ChangeAboutme>
+      </AboutmeWrapper>
 
-        <Btns onClick={openModalChange}>비밀번호 변경</Btns>
-        {showChangeModal ? (
-          <ModalBackdrop onClick={openModalChange}>
-            <Modal onClick={(event) => event.stopPropagation()}>
-              <h3>비밀번호 변경 모달</h3>
-              <input
-                type="text"
-                value={newPassword}
-                onChange={passwordChange}
-              />
-              <Btns onClick={() => handlePasswordChange()}>
-                비밀번호 변경하기
-              </Btns>
-              <Btns onClick={openModalChange}>변경취소하기</Btns>
-            </Modal>
-          </ModalBackdrop>
-        ) : null}
-      </div>
+      <ChangeUser>
+        <div className="container">
+          <Btns onClick={openModalOut}>회원탈퇴</Btns>
+          {showOutModal ? (
+            <ModalBackdrop onClick={openModalOut}>
+              <Modal onClick={(event) => event.stopPropagation()}>
+                <h3>진짜로 탈퇴하실건가요..</h3>
+                <Btns onClick={handleDelete(myemail)}>진심탈퇴 하기</Btns>
+                <Btns onClick={openModalOut}>탈퇴 취소하기</Btns>
+              </Modal>
+            </ModalBackdrop>
+          ) : null}
+
+          <Btns onClick={openModalChange}>비밀번호 변경</Btns>
+          {showChangeModal ? (
+            <ModalBackdrop onClick={openModalChange}>
+              <Modal onClick={(event) => event.stopPropagation()}>
+                <h3>비밀번호 변경 모달</h3>
+                <input
+                  type="text"
+                  value={newPassword}
+                  onChange={passwordChange}
+                  onClick={() => handlePasswordChange()}
+                />
+                <button onClick={() => handlePasswordChange()}>
+                  비밀번호 변경하기
+                </button>
+                <button onClick={openModalChange}>변경취소하기</button>
+              </Modal>
+            </ModalBackdrop>
+          ) : null}
+        </div>
+      </ChangeUser>
     </ProfileEditWrapper>
   );
 };
